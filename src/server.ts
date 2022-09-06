81dda0bc6ad18682e96fa84d3126a1dd4a719b0f
Heytor Cavalcanti <heytor01@live.com>
2022-09-05 22:03:39 -0300

    feat: added dotenv in server file

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import express from 'express';
 import cors from 'cors';
 
@@ -9,4 +11,4 @@ app.use(express.json({ limit: '5mb' }));
 app.use(cors());
 app.use(routes);
 
-app.listen(process.env.PORT || 4000, () => console.log('server started!'));
+app.listen(process.env.PORT || 4000, () => console.log(`Server started in ${process.env.PORT} port!`));
